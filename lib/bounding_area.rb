fddb14d1d57c7be444a784e4d3a2cbdc2791b87b
Andrew <akcrono@gmail.com>
2014-09-06 16:08:11 -0400

    working program

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,14 @@
 class BoundingArea
+  attr_reader :rectangles
+
+  def initialize(rectangles)
+    @rectangles = rectangles
+  end
+
+  def contains_point?(x_coord, y_coord)
+    rectangles.each do |rectangle|
+      return true if rectangle.contains_point?(x_coord, y_coord)
+    end
+    false
+  end
 end
--- lib/bounding_box.rb
+++ lib/bounding_box.rb
@@ -1,2 +1,27 @@
+require_relative 'bounding_box'
+
 class BoundingBox
+  attr_reader :left, :bottom, :height, :width
+
+  def initialize(left, bottom, width, height)
+    @left = left
+    @bottom = bottom
+    @height = height
+    @width = width
+  end
+
+  def right
+    left + width
+  end
+
+  def top
+    bottom + height
+  end
+
+  def contains_point?(x_coord, y_coord)
+    if x_coord.between?(left, right) && y_coord.between?(bottom, top)
+      return true
+    end
+    false
+  end
 end
